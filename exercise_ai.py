80358879c8ef6d478c611435231baac1a4e932fc
Constantin Mitides <mitides.constantin@gmail.com>
2020-10-14 17:58:55 -0400

    switching back to templates vs javascript

--- exercise_ai.py
+++ exercise_ai.py
@@ -35,38 +35,39 @@ def show_exercise_form():
     # level of experience
     # plan type (strenght, hypertrophy)
     # sugessted number of exercises
-    # ga_input = RequirementsForm()
-    # if ga_input.validate_on_submit():
-    #     gen_c =  gc.Genetic_Class(ga_input.lvl_exp.data,
-    #     ga_input.goal_type.data,
-    #     ga_input.no_days.data,
-    #     ga_input.no_ex.data)
+    ga_input = RequirementsForm()
+    if ga_input.validate_on_submit():
+        gen_c =  gc.Genetic_Class(ga_input.lvl_exp.data,
+        ga_input.goal_type.data,
+        ga_input.no_days.data,
+        ga_input.no_ex.data)
 
-    #     print(gen_c.usr_lvl, gen_c.goal, gen_c.no_days, gen_c.ex_len)
-    #     result_list = gen_c.start_evolution()
-    #     result_list = json.loads(result_list.to_json(orient='records'))
-        
-    #     print(type(result_list))
-    #     return render_template('exercise_result.html', results = result_list)
-    # return render_template('exerciseform.html', form=ga_input)
-    if request.method == 'POST':
-        inputs = request.json
-        # print(type(inputs))
-        # print(inputs)
-        print('recieved inputs')
-        gen_c = gc.Genetic_Class(inputs['LevelOfExp'], inputs['goalType'], inputs['NumberOfDays'], inputs['NumberOfExercises'])
+        print(gen_c.usr_lvl, gen_c.goal, gen_c.no_days, gen_c.ex_len)
         result_list = gen_c.start_evolution()
-        # TODO: Cut out the unnecessary data before sending to front end. you only want to save the top 
-        # 5 programs
-        # single_best_plan = pf.limit_one(result_list)
-        # print(type(single_best_plan))
-        # return jsonify(json.loads(result_list.to_json(orient='records')))
-        # print(type(single_best_plan))
-        # print('jsonified:::::::::: ',type(jsonify(single_best_plan)))
-        # print(len(single_best_plan))
-        # print(pf.flatten_microcyc(result_list))
-        return (jsonify(pf.flatten_microcyc(result_list)))
-    return 'lol'
+        result_list = json.loads(result_list.to_json(orient='records'))
+        
+        print(type(result_list))
+        return render_template('exercise_result.html', results = result_list)
+    return render_template('exerciseform.html', form=ga_input)
+    # ANGULAR INTERGRATION
+    # if request.method == 'POST':
+    #     inputs = request.json
+    #     # print(type(inputs))
+    #     # print(inputs)
+    #     print('recieved inputs')
+    #     gen_c = gc.Genetic_Class(inputs['LevelOfExp'], inputs['goalType'], inputs['NumberOfDays'], inputs['NumberOfExercises'])
+    #     result_list = gen_c.start_evolution()
+    #     # TODO: Cut out the unnecessary data before sending to front end. you only want to save the top 
+    #     # 5 programs
+    #     # single_best_plan = pf.limit_one(result_list)
+    #     # print(type(single_best_plan))
+    #     # return jsonify(json.loads(result_list.to_json(orient='records')))
+    #     # print(type(single_best_plan))
+    #     # print('jsonified:::::::::: ',type(jsonify(single_best_plan)))
+    #     # print(len(single_best_plan))
+    #     # print(pf.flatten_microcyc(result_list))
+    #     return (jsonify(pf.flatten_microcyc(result_list)))
+    # return 'lol'
 # @app.route('/results', methods=['POST'])
 # def results():
 #     form = request.form
